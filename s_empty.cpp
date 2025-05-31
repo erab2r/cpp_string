23fdec223f2c0caefe5fc328163df2c786774cb5
erab2r <shkera213@gmail.com>
2025-06-01 02:03:09 +0600

    3rd

---
+++ s_empty.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s = "Israt Jahan Era Israt Jahan Era Israt Jahan Era Israt Jahan Era ";
+    if(s.empty()==true){
+        cout << "Empty" << endl;
+    }
+    else {
+        cout << "Not Empty" << endl;
+    }
+    return 0;
+}
