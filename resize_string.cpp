4c728bf3ce92968b149a6bf899a663e96d07334b
erab2r <shkera213@gmail.com>
2025-06-01 02:06:03 +0600

    4th

---
+++ resize_string.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s = "Israt Jahan Era Israt Jahan Era Israt Jahan Era Israt Jahan Era ";
+    s.resize(5);
+    cout << s << endl;
+    return 0;
+}
