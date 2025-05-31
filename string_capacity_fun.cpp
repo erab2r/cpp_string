6bcba7680dd8a852db60f611546f8b232c4efd95
erab2r <shkera213@gmail.com>
2025-06-01 01:59:46 +0600

    2nd

--- string_capacity_fun.cpp
+++ string_capacity_fun.cpp
@@ -1,8 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s = "Israt Jahan Era";
+    string s = "Israt Jahan Era Israt Jahan Era Israt Jahan Era Israt Jahan Era ";
     cout << s.size() << endl;
-     cout << s.max_size() << endl;
+    cout << s.max_size() << endl;
+    cout << s.capacity() << endl;
     return 0;
 }
