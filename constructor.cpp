ee6cc1f8ec56e88dbe72e183a8141775719e6b11
erab2r <shkera213@gmail.com>
2025-06-02 02:53:23 +0600

    7th

--- constructor.cpp
+++ constructor.cpp
@@ -1,8 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s("Hello");
+    // string s("Hello",3);
     // cin >> s;
+    // string s = "Hello world";
+    // string t(s,2);
+    string s(5,'E');
     cout << s << endl;
 
     return 0;
