07c75e247d63b04ed1e0f1dbd256fd57c5c9deb0
erab2r <shkera213@gmail.com>
2025-06-02 00:41:46 +0600

    5th

---
+++ string_modifier.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s = "Israt era ";
+    string s2 = "era";
+    // s.append(s2);
+    //  s.push_back('e');
+    // s.pop_back();
+    // s.assign("ERA");
+    // s.erase(3,2);
+    // s.replace(3,2,"HB");
+    
+    cout << s << endl; 
+    return 0;
+}
